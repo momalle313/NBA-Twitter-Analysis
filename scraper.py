571105e48e24f8c06717fde86b332cd1f43bbea6
EC2 Default User <jdumford@nd.edu>
2018-04-22 22:13:15 +0000

    adding all the data

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import sys
 import tweepy
+import datetime
 
 ### Michael O'Malley, Jacob Dumford, Gregory Nemecek
 ### Social Sensing and Cyber Physical Systems
@@ -42,13 +43,15 @@ class StreamListener(tweepy.StreamListener):
 
 	# When a status is found, record the necessary information
 	def on_status(self, status):
-		if not status.retweeted and 'RT @' not in status.text.encode('utf 8'):
-			self.id += 1			
-			print "Hit: " + str(self.id)						
-			self.output.write(str(self.id) + '\t')
-			self.output.write(status.text.encode('utf 8').replace('\n','') + '\t')
-			self.output.write(str(status.created_at) + '\t')
-			self.output.write(str(status.coordinates) + '\n')
+                if datetime.datetime.now() > datetime.datetime(2018, 4, 22, 4):
+                        quit()
+                self.id += 1			
+                print "Hit: " + str(self.id)						
+                self.output.write(str(self.id) + '\t')
+                self.output.write(str(status.user.id_str) + '\t')
+                self.output.write(status.text.encode('utf 8').replace('\n','') + '\t')
+                self.output.write(str(status.created_at) + '\t')
+                self.output.write(str(status.coordinates) + '\n')
 
 	# Stop if an error occurs
 	def on_error(self, status_code):
@@ -60,11 +63,22 @@ class StreamListener(tweepy.StreamListener):
 if __name__ == "__main__":
 
 	# Consumer keys and access token
-	consumer_key = 'l6h9Z40w3SM3QdFrRWH2hKVoL'
-	consumer_secret = 'tPB3xDHDTnr3FjgV2pRKnc4mMBQyviPWyVmLqgYU4WaBc9BVI0'
-	access_token = '960373416892170240-omnMrJJ2NDSkSL4HTEMEqgQlpIPsjQw'
-	access_token_secret = 'KXs1K4jgmSe5pWCCWEhNZxiUR7wEkeo9KxNrSfhG7bk8a'
-	 
+        if sys.argv[1] == "Bulls":
+                consumer_key = 'mpYkPEvyCOmgYNhAolUy2vkjR'
+                consumer_secret = 'EeGj9wOcvGdJID7BgN1YhwSrhS21BlLNfgQ0oszenDxxHPvlxt'
+                access_token = '918814989321949186-CLvmoUgBZ5DM148E4UmE88sqXSxPYmY'
+                access_token_secret = 'ajQQjV3G2kEs42Y7F12UT4swJiQdOvLeOXch6gweWWIQm'
+        elif sys.argv[1] == "Heat":
+                consumer_key = 'l6h9Z40w3SM3QdFrRWH2hKVoL'
+                consumer_secret = 'tPB3xDHDTnr3FjgV2pRKnc4mMBQyviPWyVmLqgYU4WaBc9BVI0'
+                access_token = '960373416892170240-omnMrJJ2NDSkSL4HTEMEqgQlpIPsjQw'
+                access_token_secret = 'KXs1K4jgmSe5pWCCWEhNZxiUR7wEkeo9KxNrSfhG7bk8a'
+        else:
+                consumer_key = 'KOBww3arz3J0v0fD81ucdkm51'
+                consumer_secret = 'B0ttUv182vk1LrGtKrMi2kwo73KdLYJaqMMsDDocWJ8GB3yrBZ'
+                access_token = '888969227046191105-iNSWv2NJpvoHLbVNHIws2W7Wz1nXYmB'
+                access_token_secret = 'gR5yfLZIAFMuWkHmjNwNYAUK6TID68OXLwUXCjughWtJ2'
+
 	# OAuth process
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
