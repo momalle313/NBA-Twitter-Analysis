a86c25f812fc58783f6392160f5f46edd4438f02
michael <momalle3@nd.edu>
2018-05-02 00:21:19 -0400

    Finished Tweets vs Time graphs

--- graph-results.py
+++ graph-results.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import time
+import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,6 +15,7 @@ import matplotlib.pyplot as plt
 
 ### Main Execution ###
 
+
 if __name__=="__main__":
 
 	# Read in training data
@@ -26,48 +28,80 @@ if __name__=="__main__":
 	else:
 		data = pd.read_csv(str(sys.argv[1]) + '-data-clean.txt', sep='\t')
 
-	# Record start time metrics
-	start_time = data['timestamp'].iloc[0].split()[1].split(':')
-	
-	start_hour = start_time[0]
-	start_min = start_time[1]
-	start_sec = start_time[2]
+	# Set times for the recorded games
+	if str(sys.argv[1]) == 'Heat':
+		starttimes = ['2018-03-21 23:30:00','2018-03-24 00:00:00','2018-04-03 23:30:00','2018-04-04 23:30:00','2018-04-09 23:30:00','2018-04-12 00:00:00']
+	elif str(sys.argv[1]) == 'Bulls':
+		starttimes = ['2018-03-22 00:00:00','2018-03-24 00:00:00','2018-03-24 23:00:00','2018-03-28 00:00:00','2018-04-04 00:00:00','2018-04-08 00:00:00','2018-04-09 23:30:00','2018-04-12 00:00:00']
+	else:
+		starttimes = ['2018-03-21 02:30:00','2018-03-23 00:00:00','2018-03-25 00:00:00','2018-03-28 00:00:00','2018-04-04 00:00:00','2018-04-06 00:00:00','2018-04-08 00:30:00','2018-04-11 02:30:00','2018-04-12 02:30:00']
+			
 
-	interval = 5
+	# Examine each game in the given teams data
+	for time in starttimes:
 
-	times = []
-	tot_tweets = []
-	labels = []
-	tot_time = 0
-	tweet_count = 0
-	for index, row in data.iterrows():
+		# Record start time and end time metrics
+		a = time.replace('-',' ').replace(':',' ').split()
+		a = [int(x) for x in a]
+		start_time = datetime.datetime(a[0],a[1],a[2],a[3],a[4],a[5])
+		if a[3] > 21:
+			end_time = datetime.datetime(a[0],a[1],a[2]+1,(a[3]-24)+2,a[4],a[5])
+		else:
+			end_time = datetime.datetime(a[0],a[1],a[2],a[3]+2,a[4],a[5])
+		
+		minutes = start_time.minute
 
-		tweet_count += 1
-		time = row['timestamp'].split()[1].split(':')
+		# Initialize loop variables
+		interval = 2
+		times = []
+		tot_tweets = []
+		labels = []
+		tot_time = 0
+		tweet_count = 0
 
-		if abs(int(time[0]) - int(start_hour)) > 1:
-			print "New game"
+		# Loop through all the data
+		for index, row in data.iterrows():
 
-		if abs(int(time[1]) - int(start_min)) == interval:
+			# Parse time of data point
+			b = row['timestamp'].replace('-',' ').replace(':',' ').split()
+			b = [int(x) for x in b]
+			time = datetime.datetime(b[0],b[1],b[2],b[3],b[4],b[5])
 
-			times.append(tot_time)
-			tot_time += interval
-			labels.append(str(time[0]) + ':' + str(time[1]))
-			start_min = time[1]
+			# Skip if not at time yet
+			if start_time > time:
+				continue
 
-			tot_tweets.append(tweet_count)		
-			tweet_count = 0
+			# Break if past endtime
+			if end_time < time:
+				break
 
-		if tot_time >= 100:
-			break
+			# Add to tweet_count
+			tweet_count += 1
 
-	plt.plot(times, tot_tweets, 'ro')
-	plt.title('Number of Tweets per ' + str(interval) + ' Minute Interval')
-	plt.xlabel('Time Interval')
-	plt.ylabel('Number of Tweets')
-	plt.xticks(times, labels, rotation='vertical')
-	plt.show()
-	plt.close()
+			# If interval has been reach, start a new bin
+			if abs(time.minute - minutes) >= interval:
+
+				# Record time variables
+				times.append(tot_time)
+				tot_time += interval
+				labels.append(str(time.hour) + ':' + str(time.minute))
+				minutes = time.minute
+				tot_tweets.append(tweet_count)		
+				tweet_count = 0
+
+		# Format plot
+		plt.figure(figsize=(12,6))
+		plt.plot(times, tot_tweets)
+		plt.title(str(sys.argv[1]) + ' Game: ' + str(start_time))
+		plt.xlabel('Time Interval')
+		plt.ylabel('Number of Tweets')
+		plt.xticks(times, labels, rotation='vertical')
+		plt.grid(which='minor', alpha=0.2)
+		plt.grid(which='major', alpha=0.5)
+		plt.tight_layout(w_pad=1.5, h_pad=1.5)
+
+		# Show plot
+		plt.savefig('./graphs/'+str(sys.argv[1])+' '+str(start_time)+'.png')
 
 
 
